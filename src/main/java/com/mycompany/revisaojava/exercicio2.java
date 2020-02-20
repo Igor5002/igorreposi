5136574a113a51247e276b26e79764af3b0b5d05
igor.valgoi <igor.valgoi@192.168.56.1>
2020-02-20 16:54:22 -0600

    Exercicio2

---
+++ src/main/java/com/mycompany/revisaojava/exercicio2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.revisaojava;
+
+/**
+ *
+ * @author igor.valgoi
+ */
+public class exercicio2 {
+   
+    public static void main (String[]arqs){
+        String t1 = "Texto 1";
+        String t2 = "Texto 2";
+        imprimir(t1, t2);
+       
+       
+        
+    
+      
+
+    }
+
+    private static void imprimir(String texto1,String texto2){
+       System.out.println(texto1);
+       System.out.println(texto2);
+       
+   }
+}
